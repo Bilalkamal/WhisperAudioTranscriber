42fc535cea8bae761a64c8045db3dfaeebe622ca
Bilal <bilalkamal@users.noreply.github.com>
2024-05-16 10:08:36 +0300

    Added notification for when the script is done running.

--- whisper_converter.py
+++ whisper_converter.py
@@ -33,6 +33,16 @@ def convert_m4a_to_mp3(source_directory):
         m4a_audio.export(mp3_filename, format="mp3")
         os.remove(filename)  
 
+def convert_wav_to_mp3(source_directory):
+    wav_files = [f for f in os.listdir(source_directory) if f.endswith(".wav")]
+    for filename in tqdm(wav_files, desc="Converting .wav to .mp3"):
+        wav_audio = AudioSegment.from_file(filename, "wav")
+        mp3_filename = f"{filename[:-4]}.mp3"
+        wav_audio.export(mp3_filename, format="mp3")
+        os.remove(filename)
+
+        
+
 def transcribe_audio(source_directory, model, mp3_directory, txt_directory):
     mp3_files = [f for f in os.listdir(source_directory) if f.endswith(".mp3")]
     all_texts = ""
@@ -84,6 +94,8 @@ def main():
 
     end_time = time.time()
     print(f"Process completed in {end_time - start_time:.2f} seconds.")
+    for _ in range(3):
+        os.system("afplay /System/Library/Sounds/Submarine.aiff")
 
 if __name__ == "__main__":
     main()
